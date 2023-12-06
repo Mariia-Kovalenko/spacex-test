c0e0800e2ee96bf756a8bc6b7ff353c1f07cd69d
Mariia-Kovalenko <mariya.kovalenko.2000@gmail.com>
2023-12-06 19:35:25 +0200

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './components/pages/Home/Home';
+import Favourites from './components/pages/Favourites/Favourites';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favourites' element={<Favourites />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
