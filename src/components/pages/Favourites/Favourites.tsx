f699f2bb6b0a21f5e5b1f9381f34ec644b54b99d
Mariia-Kovalenko <mariya.kovalenko.2000@gmail.com>
2023-12-07 11:54:05 +0200

    finished favourites page layout styling

--- src/components/pages/Favourites/Favourites.tsx
+++ src/components/pages/Favourites/Favourites.tsx
@@ -1,7 +1,45 @@
+import Button from "../../atoms/Button/Button";
+import IconButton from "../../atoms/IconButton/IconButton";
+import Card from "../../molecules/Card/Card";
+import "./Favourites.scss";
+
 export default function Favourites() {
+    const favourites = [{ title: "item1" }, { title: "item2" }];
+
     return (
         <div className="favourites">
-            Favourites
+            <div className="favourites__banner">
+                <h1 className="title">Favourites</h1>
+                <img src="./slider-3.jpg" alt="banner" />
+            </div>
+            <div className="favourites__list container">
+                <button className="clear">Clear All</button>
+                <div className="cards">
+                    {favourites.map(({ title }) => {
+                        return (
+                            <Card
+                                image={`./slider-1.jpg`}
+                                key={`card-${Math.random()}`}
+                                title={title}
+                                content={"description"}
+                                children={[
+                                    <Button
+                                        key="jk"
+                                        text={"Buy"}
+                                        onClick={() => {}}
+                                    />,
+                                    <IconButton
+                                        key="erfv"
+                                        icon={"./Delete.svg"}
+                                        alt="like"
+                                        onClick={() => {}}
+                                    />,
+                                ]}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
         </div>
-    )
+    );
 }
